cfb844be4dbc68b6520a35d3ee4c210d6e39b0fd
epotter <eric.b.potter@gmail.com>
2017-02-13 21:09:59 -0500

    Basic Working version

--- EightQueens/Program.cs
+++ EightQueens/Program.cs
@@ -10,6 +10,43 @@ namespace EightQueens
     {
         static void Main(string[] args)
         {
+            Solve();
+
+            Console.WriteLine("Press Any Key To Continue");
+            Console.ReadKey();
+        }
+
+        public static void Solve()
+        {
+
+            var queens = BuildBoard(4);
+
+            queens[Queen.BoardSize - 1].Solve();
+
+            DisplayResults(queens);
+        }
+
+        private static Queen[] BuildBoard(int size)
+        {
+            Queen.BoardSize = size;
+
+            var queens = new Queen[Queen.BoardSize];
+
+            queens[0] = new Queen(null, 1);
+            for (var i = 1; i < Queen.BoardSize; i++)
+            {
+                queens[i] = new Queen(queens[i - 1], i + 1);
+            }
+
+            return queens;
+        }
+
+        private static void DisplayResults(Queen[] queens)
+        {
+            foreach (var queen in queens)
+            {
+                Console.WriteLine($"({queen._column}, {queen.Row})");
+            }
         }
     }
 }
---
+++ EightQueens/Queen.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightQueens
+{
+    public class Queen
+    {
+        public int Row;
+        public static int BoardSize;
+
+        private Queen _nextQueen;
+        public int _column;
+
+        public Queen(Queen nextQueen, int column)
+        {
+            this._nextQueen = nextQueen;
+            this._column = column;
+            this.Row = 1;
+        }
+        public void MoveNext()
+        {
+            this.Row++;
+        }
+
+        public bool CanPositionBeAttacked(int possibleRow, int possibleColumn)
+        {
+            if (this.Row == possibleRow)
+            {
+                return true;
+            }
+            else if (this.IsOnDiagonal(possibleRow, possibleColumn))
+            {
+                return true;
+            }
+            else if (this._nextQueen != null)
+            {
+                return this._nextQueen.CanPositionBeAttacked(possibleRow, possibleColumn);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool IsOnDiagonal(int possibleRow, int possibleColumn)
+        {
+            var columnDiff = possibleColumn - this._column;
+            if (possibleRow - columnDiff == this.Row ||
+               possibleRow + columnDiff == this.Row)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Solve()
+        {
+            if (_nextQueen == null)
+            {
+                return true;
+            }
+
+            _nextQueen.Solve();
+
+            while (_nextQueen.CanPositionBeAttacked(this.Row, this._column) ||
+                  this.Row > Queen.BoardSize)
+            {
+                this.MoveNext();
+
+                if (this.Row > Queen.BoardSize)
+                {
+                    this.Row = 1;
+                    _nextQueen.MoveNext();
+                    _nextQueen.Solve();
+                }
+            }
+
+            return true;
+        }
+    }
+}
